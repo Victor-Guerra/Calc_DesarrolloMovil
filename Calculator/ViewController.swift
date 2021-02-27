5d8934f3ad88fc3ee0be72ee4c429c9ab77e0670
ei <simon>
2021-02-26 20:39:04 -0600

    Added functionality v0.1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,7 +13,57 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private static var current_operation = [String]();
+    private static var operandBuffer = [Float]();
+    @IBOutlet weak var DisplayLabel: UILabel!
+    
+    @IBAction func processNumber(_ sender: UIButton) {
+        if let numberText = sender.titleLabel?.text {
+            if (DisplayLabel.text == "0"){
+                DisplayLabel.text = numberText
+            }
+            else {
+                DisplayLabel.text = DisplayLabel.text! + numberText
+            }
+        }
+    }
+    
+    @IBAction func processOperation(_ sender: UIButton) {
+        if let operatorText = sender.titleLabel?.text {
+            ViewController.current_operation
+                .append(operatorText);
+            ViewController.operandBuffer
+                .append((DisplayLabel.text! as NSString).floatValue);
+            DisplayLabel.text = "";
+        }
+    }
+    
+    @IBAction func processEquals(_ sender: UIButton) {
+        ViewController.operandBuffer.append((DisplayLabel.text! as NSString).floatValue);
+        var result: Float = 0.0;
+        for operand in ViewController.operandBuffer {
+            var i = 0;
+            switch ViewController.current_operation[i] {
+            case "+":
+                result = result + operand;
+                break;
+            case "-":
+                result = result - operand;
+                break;
+            case "*":
+                result = result * operand;
+                break;
+            case "/":
+                result = result / operand;
+                break;
+            default:
+                break;
+                
+            }
+            i += 1;
+        }
+    }
+    
 }
 
